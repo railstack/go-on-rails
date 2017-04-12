1c520133c3d1b2361be9bad84e202e3d2e1c5f77
B1nj0y <v@joyv.net>
2017-04-12 22:09:48 +0800

    change gor_generator.rb indent 4 to 2

--- lib/generators/gor/gor_generator.rb
+++ lib/generators/gor/gor_generator.rb
@@ -5,90 +5,90 @@ class GorGenerator < Rails::Generators::Base
   class_option :only_models, type: :boolean, default: false, aliases: '-o', description: "only generate models"
 
   def generate_gor
-      env_names = %W(dev development pro production test)
-      unless env_names.include? env_name
-          printf("Invalid env argument: Not any of %p\n\n", env_names)
-          exit
+    env_names = %W(dev development pro production test)
+    unless env_names.include? env_name
+      printf("Invalid env argument: Not any of %p\n\n", env_names)
+      exit
+    end
+
+    rails_env = case env_name
+                when "dev"
+                  "development"
+                when "pro"
+                  "production"
+                else
+                  env_name
+                end
+
+    models = options[:models]
+    if models.empty?
+      models = get_all_models "app/models"
+    else
+      models.map!(&:camelize)
+    end
+    puts "The models: #{models} and Rails env [#{rails_env}] will be used to generate Golang App!"
+
+    models.each do |m|
+      begin
+        klass = m.split('::').inject(Object) { |kls, part| kls.const_get(part) }
+        if klass < ActiveRecord::Base && !klass.abstract_class?
+          @model_name = klass.to_s
+          convertor = GoOnRails::Convertor.new(klass, models)
+          @struct_info = convertor.convert
+          template "gor_model.go.erb", "go_app/models/gor_#{@model_name.underscore}.go"
+        end
+      rescue Exception => e
+        puts "Failed to convert the model [#{m}]: #{e.message}"
       end
+    end
 
-      rails_env = case env_name
-      when "dev"
-          "development"
-      when "pro"
-          "production"
-      else
-          env_name
-      end
+    unless options[:only_models]
+      # generate the main.go
+      copy_file "main.go", "go_app/main.go"
 
-      models = options[:models]
-      if models.empty?
-          models = get_all_models "app/models"
-      else
-          models.map!(&:camelize)
-      end
-      puts "The models: #{models} and Rails env [#{rails_env}] will be used to generate Golang App!"
+      # generate program for database connection
+      @db_config = {}
+      create_database_config(rails_env)
+      template "db.go.erb", "go_app/models/db.go"
 
-      models.each do |m|
-          begin
-              klass = m.split('::').inject(Object) { |kls, part| kls.const_get(part) }
-              if klass < ActiveRecord::Base && !klass.abstract_class?
-                  @model_name = klass.to_s
-                  convertor = GoOnRails::Convertor.new(klass, models)
-                  @struct_info = convertor.convert
-                  template "gor_model.go.erb", "go_app/models/gor_#{@model_name.underscore}.go"
-              end
-          rescue Exception => e
-              puts "Failed to convert the model [#{m}]: #{e.message}"
-          end
-      end
+      # generate the controllers and views dir
+      copy_file "home_controller.go", "go_app/controllers/home_controller.go"
+      copy_file "index.tmpl", "go_app/views/index.tmpl"
+      copy_file "favicon.ico", "go_app/public/favicon.ico"
+    end
 
-      unless options[:only_models]
-          # generate the main.go
-          copy_file "main.go", "go_app/main.go"
-
-          # generate program for database connection
-          @db_config = {}
-          create_database_config(rails_env)
-          template "db.go.erb", "go_app/models/db.go"
-
-          # generate the controllers and views dir
-          copy_file "home_controller.go", "go_app/controllers/home_controller.go"
-          copy_file "index.tmpl", "go_app/views/index.tmpl"
-          copy_file "favicon.ico", "go_app/public/favicon.ico"
-      end
-
-      # use gofmt to prettify the generated Golang files
-      gofmt_go_files
+    # use gofmt to prettify the generated Golang files
+    gofmt_go_files
   end
 
   private
 
   def get_all_models model_dir
-      Dir.chdir(model_dir) do
-          Dir["**/*.rb"]
-      end.map { |m| m.sub(/\.rb$/,'').camelize }
+    Dir.chdir(model_dir) do
+      Dir["**/*.rb"]
+    end.map { |m| m.sub(/\.rb$/,'').camelize }
   end
 
   def create_database_config rails_env
-      db_conf = Rails.configuration.database_configuration[rails_env]
-      case db_conf["adapter"]
-      when "sqlite3"
-          @db_config[:driver_name] = "sqlite3"
-          @db_config[:dsn] = "../" + db_conf["database"]
-          @db_config[:driver_package] = "_ \"github.com/mattn/go-sqlite3\""
-      when "mysql2"
-          @db_config[:driver_name] = "mysql"
-          # MySQL DSN format: username:password@protocol(address)/dbname?param=value
-          # See more: https://github.com/go-sql-driver/mysql
-          format = "%s:%s@%s/%s?charset=%s&parseTime=True&loc=Local"
-          @db_config[:dsn] = sprintf(format, *db_conf.values_at("username", "password", "host", "database", "encoding"))
-          @db_config[:driver_package] = "_ \"github.com/go-sql-driver/mysql\""
-      when "postgresql"
-          @db_config[:driver_name] = "postgres"
-          format = "host=%s user=%s dbname=%s sslmode=disable password=%s"
-          @db_config[:dsn] = sprintf(format, *db_conf.values_at("host", "username", "database", "password"))
-          @db_config[:driver_package] = "_ \"github.com/lib/pq\""
-      end
+    db_conf = Rails.configuration.database_configuration[rails_env]
+    case db_conf["adapter"]
+    when "sqlite3"
+      @db_config[:driver_name] = "sqlite3"
+      @db_config[:dsn] = "../" + db_conf["database"]
+      @db_config[:driver_package] = "_ \"github.com/mattn/go-sqlite3\""
+    when "mysql2"
+      @db_config[:driver_name] = "mysql"
+      # MySQL DSN format: username:password@protocol(address)/dbname?param=value
+      # See more: https://github.com/go-sql-driver/mysql
+      format = "%s:%s@%s/%s?charset=%s&parseTime=True&loc=Local"
+      @db_config[:dsn] = sprintf(format, *db_conf.values_at("username", "password", "host", "database", "encoding"))
+      @db_config[:driver_package] = "_ \"github.com/go-sql-driver/mysql\""
+    when "postgresql"
+      @db_config[:driver_name] = "postgres"
+      format = "host=%s user=%s dbname=%s sslmode=disable password=%s"
+      @db_config[:dsn] = sprintf(format, *db_conf.values_at("host", "username", "database", "password"))
+      @db_config[:driver_package] = "_ \"github.com/lib/pq\""
+    end
   end
 
   def gofmt_go_files
