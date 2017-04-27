55c1133a8755d38f9523007d6b0c70ab09224dce
B1nj0y <v@joyv.net>
2017-04-27 21:53:58 +0800

    add some model migrations

---
+++ db/migrate/20170411090919_create_appointments.rb
@@ -0,0 +1,11 @@
+class CreateAppointments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :appointments do |t|
+      t.datetime :appointment_date
+      t.integer :phisician_id
+      t.integer :patient_id
+
+      t.timestamps
+    end
+  end
+end
