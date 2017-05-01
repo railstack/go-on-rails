01138895192a6035e8e81c641713b3e0d09d8111
B1nj0y <v@joyv.net>
2017-05-01 11:41:36 +0800

    add comments to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Here are some of famous physicians in ancient China: 李时珍, 张仲景, 扁鹊
 Physician.create([{ name: 'ShizhenLi' }, { name: 'ZhongjingZhang' }, { name: 'QueBian' } ])
 
+# and 华佗,
 p = Physician.create(name: 'TuoHua' )
+# He ever treated some named patients in his time, Three Kingdoms:
+# 曹操, 关羽, 陈登, 周泰
 p.patients.create([{ name: 'CaoCao' }, { name: 'YuGuan' }, { name: 'DengChen' }, { name: 'TaiZhou' }])
-
