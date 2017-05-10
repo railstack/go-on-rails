fdd84f95db33bce48f3922e5d677418e48577b3a
B1nj0y <v@joyv.net>
2017-05-10 14:39:27 +0800

    migrate gorilla suite to Gin

--- lib/generators/gor/templates/main.go
+++ lib/generators/gor/templates/main.go
@@ -2,28 +2,26 @@ package main
 
 import (
 	"flag"
-	"log"
-	"net/http"
-	"os"
 
 	c "./controllers"
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
+	"gopkg.in/gin-gonic/gin.v1"
 )
 
 func main() {
 	// The app will run on port 3000 by default, you can custom it with the flag -port
 	servePort := flag.String("port", "3000", "Http Server Port")
 	flag.Parse()
-	log.Printf("The application starting on the port: [%v]\n", *servePort)
 
-	// Here we are instantiating the gorilla/mux router
-	r := mux.NewRouter()
-	// Then we bind some route to some handler(controller action)
-	r.HandleFunc("/", c.HomeHandler)
+	// Here we are instantiating the router
+	r := gin.Default()
+	// Switch to "release" mode in production
+	// gin.SetMode(gin.ReleaseMode)
+	r.LoadHTMLGlob("views/*")
 	// Create a static assets router
-	r.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./public/"))))
-
+	// r.Static("/assets", "./public/assets")
+	r.StaticFile("/favicon.ico", "./public/favicon.ico")
+	// Then we bind some route to some handler(controller action)
+	r.GET("/", c.HomeHandler)
 	// Let's start the server
-	http.ListenAndServe(":"+*servePort, handlers.LoggingHandler(os.Stdout, r))
+	r.Run(":" + *servePort)
 }
