3b5d3adbac3c64ca179f4b0c138979296616e889
B1nj0y <v@joyv.net>
2017-09-07 10:26:16 +0800

    run annotate

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: pictures
+#
+#  id             :integer          not null, primary key
+#  name           :string(255)
+#  url            :string(255)
+#  imageable_id   :integer
+#  imageable_type :string(255)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class Picture < ApplicationRecord
   belongs_to :imageable, polymorphic: true
 end
