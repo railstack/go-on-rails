4fc5a719462f4fd6490dc6a89c11f0da3d44511a
B1nj0y <v@joyv.net>
2017-09-11 22:31:17 +0800

    update the seeds script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Clear the records firstly
+Physician.delete_all
 
 # Here are some of famous physicians in ancient China: 李时珍, 张仲景, 扁鹊
 Physician.create([
@@ -18,3 +13,5 @@ p = Physician.create(name: 'TuoHua', introduction: "A very famous doctor in anci
 # He ever treated some named patients in his time, Three Kingdoms:
 # 曹操, 关羽, 陈登, 周泰
 p.patients.create([{ name: 'CaoCao' }, { name: 'YuGuan' }, { name: 'DengChen' }, { name: 'TaiZhou' }])
+
+puts 'Physician seeded!'
